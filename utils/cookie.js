c94cbd865c1ca3d899eaf8129ee46885a538dda7
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-07-10 21:41:11 +0800

    test cookie

--- utils/cookie.js
+++ utils/cookie.js
@@ -3,9 +3,9 @@ class Cookie {
     return res.cookie('jwt', token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: 'none',
+      sameSite: 'strict',
       secure: true,
-      domain: '.mqzcuyfbct.ap-northeast-1.awsapprunner.com',
+      domain: '.onrender.com',
       path: '/',
     })
   }
@@ -13,9 +13,9 @@ class Cookie {
   clear(res) {
     return res.clearCookie('jwt', {
       httpOnly: true,
-      sameSite: 'none',
+      sameSite: 'strict',
       secure: true,
-      domain: '.mqzcuyfbct.ap-northeast-1.awsapprunner.com',
+      domain: '.onrender.com',
       path: '/',
     })
   }
