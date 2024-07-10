d04e00979574202dc83429080e204d611624d135
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-07-10 17:58:56 +0800

    test cookie

--- utils/cookie.js
+++ utils/cookie.js
@@ -7,7 +7,7 @@ class Cookie {
       // secure: process.env.NODE_ENV === 'production',
       secure: true,
       // domain: process.env.NODE_ENV === 'production' ? '.newlean14.com' : 'localhost',
-      domain: '.mqzcuyfbct.ap-northeast-1.awsapprunner.com',
+      domain: '.onrender.com',
       path: '/'
     })
   }
@@ -19,7 +19,7 @@ class Cookie {
       // secure: process.env.NODE_ENV === 'production',
       secure: true,
       // domain: process.env.NODE_ENV === 'production' ? '.newlean14.com' : 'localhost',
-      domain: '.mqzcuyfbct.ap-northeast-1.awsapprunner.com',
+      domain: '.onrender.com',
       path: '/'
     })
   }
