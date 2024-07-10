0784c7c2e2f84fbea9ee08230e63ac3dedd4353b
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-07-10 20:57:35 +0800

    test cookie

--- utils/cookie.js
+++ utils/cookie.js
@@ -2,12 +2,12 @@ class Cookie {
   store(res, token) {
     return res.cookie('jwt', token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      // httpOnly: true,
+      httpOnly: true,
       sameSite: 'none',
       // secure: process.env.NODE_ENV === 'production',
       secure: true,
       // domain: process.env.NODE_ENV === 'production' ? '.newlean14.com' : 'localhost',
-      domain: '.mqzcuyfbct.ap-northeast-1.awsapprunner.com',
+      // domain: '.mqzcuyfbct.ap-northeast-1.awsapprunner.com',
       path: '/',
       // partitioned: true
     })
@@ -15,12 +15,12 @@ class Cookie {
 
   clear(res) {
     return res.clearCookie('jwt', {
-      // httpOnly: true,
+      httpOnly: true,
       sameSite: 'none',
       // secure: process.env.NODE_ENV === 'production',
       secure: true,
       // domain: process.env.NODE_ENV === 'production' ? '.newlean14.com' : 'localhost',
-      domain: '.mqzcuyfbct.ap-northeast-1.awsapprunner.com',
+      // domain: '.mqzcuyfbct.ap-northeast-1.awsapprunner.com',
       path: '/',
       // partitioned: true
     })
