6f132daeeb3ce68ec30ec7ae5446195a7d8bc343
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-07-10 16:41:01 +0800

    test cookie

--- utils/cookie.js
+++ utils/cookie.js
@@ -2,20 +2,24 @@ class Cookie {
   store(res, token) {
     return res.cookie('jwt', token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      // httpOnly: true,
-      // sameSite: 'none',
+      httpOnly: true,
+      sameSite: 'none',
       // secure: process.env.NODE_ENV === 'production',
-      domain: process.env.NODE_ENV === 'production' ? '.newlean14.com' : 'localhost',
+      secure: true,
+      // domain: process.env.NODE_ENV === 'production' ? '.newlean14.com' : 'localhost',
+      domain: '.shoper-front.onrender.com',
       path: '/'
     })
   }
 
   clear(res) {
     return res.clearCookie('jwt', {
-      // httpOnly: true,
-      // sameSite: 'none',
+      httpOnly: true,
+      sameSite: 'none',
       // secure: process.env.NODE_ENV === 'production',
-      domain: process.env.NODE_ENV === 'production' ? '.newlean14.com' : 'localhost',
+      secure: true,
+      // domain: process.env.NODE_ENV === 'production' ? '.newlean14.com' : 'localhost',
+      domain: '.shoper-front.onrender.com',
       path: '/'
     })
   }
